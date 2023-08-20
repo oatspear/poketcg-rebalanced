b024947d5032f09b1b1b08b316400137adfaf7c7
oatspear <oatspear@gmail.com>
2023-08-20 10:15:08 +0100

    status to play area pokemon

--- src/engine/duel/effect_functions/status.asm
+++ src/engine/duel/effect_functions/status.asm
@@ -43,6 +43,7 @@ SleepEffect: ; 2c030 (b:4030)
 	lb bc, PSN_DBLPSN, ASLEEP
 	jr ApplyStatusEffect
 
+
 ApplyStatusEffect: ; 2c035 (b:4035)
 	ldh a, [hWhoseTurn]
 	ld hl, wWhoseTurn
@@ -91,6 +92,8 @@ ApplyStatusEffect: ; 2c035 (b:4035)
 	scf
 	ret
 
+
+
 ; Defending Pokémon and user become confused.
 ; Defending Pokémon also becomes Poisoned.
 FoulOdorEffect:
@@ -135,4 +138,81 @@ ParalysisIfBasicEffect:
 	call GetNonTurnDuelistVariable
 	or a
 	jp z, ParalysisEffect  ; BASIC
+	ret
+
+
+; ------------------------------------------------------------------------------
+; Play Area Status Effects
+; ------------------------------------------------------------------------------
+
+; input e: PLAY_AREA_* of the target Pokémon
+PoisonEffect_PlayArea:
+	lb bc, CNF_SLP_PRZ, POISONED
+	jr ApplyStatusEffectToPlayAreaPokemon
+
+; input e: PLAY_AREA_* of the target Pokémon
+DoublePoisonEffect_PlayArea:
+	lb bc, CNF_SLP_PRZ, DOUBLE_POISONED
+	jr ApplyStatusEffectToPlayAreaPokemon
+
+; input e: PLAY_AREA_* of the target Pokémon
+ParalysisEffect_PlayArea:
+	lb bc, PSN_DBLPSN, PARALYZED
+	jr ApplyStatusEffectToPlayAreaPokemon
+
+; input e: PLAY_AREA_* of the target Pokémon
+ConfusionEffect_PlayArea:
+	lb bc, PSN_DBLPSN, CONFUSED
+	jr ApplyStatusEffectToPlayAreaPokemon
+
+; input e: PLAY_AREA_* of the target Pokémon
+SleepEffect_PlayArea:
+	lb bc, PSN_DBLPSN, ASLEEP
+	jr ApplyStatusEffectToPlayAreaPokemon
+
+
+; assumes:
+;   - SwapTurn if needed to change to the correct play area
+; input:
+;   b: mask of status conditions to preserve on the target
+;   c: status condition to inflict to the target
+;   e: PLAY_AREA_* of the target Pokémon
+; outputs:
+;   [wNoEffectFromWhichStatus]: set with the input status condition
+;   carry: set if able to apply status
+ApplyStatusEffectToPlayAreaPokemon:
+	ld a, DUELVARS_ARENA_CARD
+	add e
+	call GetTurnDuelistVariable
+	cp $ff
+	jr z, .cant_induce_status
+	push de
+	call GetCardIDFromDeckIndex
+	ld a, e
+	pop de
+	cp CLEFAIRY_DOLL
+	jr z, .cant_induce_status
+	cp MYSTERIOUS_FOSSIL
+	jr z, .cant_induce_status
+	; Snorlax's Thick Skinned prevents it from being statused...
+	cp SNORLAX
+	jr nz, .can_induce_status
+	; ...unless already so, or if affected by Muk's Toxic Gas
+	call CheckCannotUseDueToStatus
+	jr c, .can_induce_status
+
+.cant_induce_status
+	ld a, c
+	ld [wNoEffectFromWhichStatus], a
+	call SetNoEffectFromStatus
+	or a
+	ret
+
+.can_induce_status
+	ld a, DUELVARS_ARENA_CARD_STATUS
+	add e
+	call GetTurnDuelistVariable  ; current status
+	and b  ; status condition to preserve
+	or c  ; status to apply on top
+	scf
 	ret
